e39ddf8a37047392f58fa49a5c71806b21c882a2
ohoo2454 <66541536@qq.com>
2021-02-16 21:51:27 +0800

    Refactor function test about check for row in list table

--- function_test.py
+++ function_test.py
@@ -12,6 +12,13 @@ class NewVisitorTest(unittest.TestCase):
         
     def tearDown(self):
         self.browser.quit()
+
+    def check_for_row_in_list_table(self, row_text):
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+
+        self.assertIn(row_text, [row.text for row in rows])
         
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 伊迪斯听说有一个很酷的在线待办事项应用
@@ -37,13 +44,7 @@ class NewVisitorTest(unittest.TestCase):
         # 待办事项表格中显示了“1: Buy peacock feathers”
         input_box.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows], 
-            'New to-do item did not appear in table -- its text was: \n%s' % (
-                table.text
-            )
-        )
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # 页面又显示了一个文本框，可以输入其它待办事项
         # 她输入了“Use peacock feathers to make a fly”
@@ -56,9 +57,9 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_tag_name('table')
         rows = table.find_elements_by_tag_name('tr')
 
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly', 
-            [row.text for row in rows]
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table(
+            '2: Use peacock feathers to make a fly'
         )
 
         # 伊迪斯想知道这个网站是否会记住她的清单
