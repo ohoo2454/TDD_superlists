b745f4c2f27781a0d09ed272d7f942120eb1c327
ohoo2454 <66541536@qq.com>
2021-02-16 14:14:14 +0800

    Function test now checks we can input a to-do item

--- function_test.py
+++ function_test.py
@@ -1,6 +1,7 @@
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -19,19 +20,33 @@ class NewVisitorTest(unittest.TestCase):
 
         # 她注意到网页的头部和标题都包含“TO-DO”这个词
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # 应用邀请她输入一个待办事项
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(input_box.get_attribute('placeholder'), 
+                         'Enter a to-do item')
+        
 
         # 她在一个文本框中输入了“Buy peacock feathers”
         # 伊迪斯的爱好是使用假蝇做饵钓鱼
+        input_box.send_keys('Buy peacock feathers')
 
         # 她按回车键后页面更新了
         # 待办事项表格中显示了“1: Buy peacock feathers”
+        input_box.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
 
         # 页面又显示了一个文本框，可以输入其它待办事项
         # 她输入了“Use peacock feathers to make a fly”
         # 伊迪斯做事很有条理
+        self.fail('Finish the test!')
 
         # 页面再次更新，他的清单中显示了这两个待办事项
 
