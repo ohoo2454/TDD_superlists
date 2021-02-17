8eb694e860ed05c41cf02fa2cf6e0df51e92661f
ohoo2454 <66541536@qq.com>
2021-02-17 16:48:57 +0800

    Now items are associated with list

--- lists/models.py
+++ lists/models.py
@@ -5,3 +5,9 @@ from django.db import models
 class Item(models.Model):
 
     text = models.TextField(default='')
+    list = models.ForeignKey(to='List', default=None)
+
+
+class List(models.Model):
+
+    text = models.TextField(default='')
