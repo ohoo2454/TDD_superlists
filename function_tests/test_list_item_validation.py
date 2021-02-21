b226ac41fa8450e8da9ab2ebdf678e5fab512a34
ohoo2454 <66541536@qq.com>
2021-02-21 21:54:19 +0800

    Refactor and add new FT to test errors on page are cleared

--- function_tests/test_list_item_validation.py
+++ function_tests/test_list_item_validation.py
@@ -2,6 +2,10 @@ from .base import FunctionTest
 
 
 class ItemValidationTest(FunctionTest):
+
+    def get_error_element(self):
+
+        return self.browser.find_element_by_css_selector('.has-error')
     
     def test_cannot_add_empty_list_items(self):
 
@@ -12,8 +16,8 @@ class ItemValidationTest(FunctionTest):
 
         # 首页刷新了，显示一个错误信息
         # 提示待办事项不能为空
-        error = self.browser.find_element_by_css_selector('.has-error')
-        self.assertEqual(error.text, "You can't have an empty list item")
+        error = self.get_error_element()
+        self.assertTrue(error.is_displayed())
 
         # 她输入一些文字，然后再次提交，这次没问题了
         self.get_item_input_box().send_keys('Buy milk\n')
@@ -24,8 +28,8 @@ class ItemValidationTest(FunctionTest):
 
         # 在清单页面她看到了一个类似的错误信息
         self.check_for_row_in_list_table('1: Buy milk')
-        error = self.browser.find_element_by_css_selector('.has-error')
-        self.assertEqual(error.text, "You can't have an empty list item")
+        error = self.get_error_element()
+        self.assertTrue(error.displayed())
 
         # 输入文字之后就没问题了
         self.get_item_input_box().send_keys('Make tea\n')
@@ -44,5 +48,20 @@ class ItemValidationTest(FunctionTest):
 
         # 她看到一条有帮助的错误消息
         self.check_for_row_in_list_table('1: Buy wellies')
-        error = self.browser.find_element_by_css_selector('.has-error')
-        self.assertEqual(error.text, "You've already got this in your list")
+        error = self.get_error_element()
+        self.assertTrue(error.is_displayed())
+
+    def test_error_messages_are_cleared_on_input(self):
+
+        # 伊迪斯新建一个清单，但方法不当，所以出现了一个验证错误
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys(' \n')
+        error = self.get_error_element()
+        self.assertTrue(error.is_displayed())
+
+        # 为了消除错误消息，她开始在输入框中输入内容
+        self.get_item_input_box().send_keys('a')
+
+        # 看到错误消息消失了，她很开心
+        error = self.get_error_element()
+        self.assertFalse(error.is_displayed())
