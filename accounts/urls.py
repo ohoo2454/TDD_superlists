d67c39a8d4367122d73fa40764e1d8b4f73dd3f6
ohoo2454 <66541536@qq.com>
2021-02-24 16:10:20 +0800

    Finish server authentication

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
+from django.contrib import auth
 
 from accounts import views
 
 urlpatterns = [
     url(r'^login$', views.persona_login, name='persona_login'),
+    url(r'^logout$', auth.logout, {'next_page': '/'}, name='logout'),
 ]
